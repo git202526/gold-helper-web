ec88947dc0811de44a8856dfd60464a02afc54eb
git202526 <gtx2012@vip.qq.com>
2026-09-15 15:17:48 +0800

    V4.24 发布: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 黄金建仓助手 V4.23 —— Service Worker：离线缓存 */
-var CACHE = 'gold-assistant-v4.23-'+Date.now();
+/* 黄金建仓助手 V4.24 —— Service Worker：离线缓存 */
+var CACHE = 'gold-assistant-v4.24-'+Date.now();
 var ASSETS = [
   './',
   './index.html',
